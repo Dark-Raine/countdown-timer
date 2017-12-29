31a0915754c58be99b094506c11796b4e16cebcc
Dark-Raine <axzonexgamer@gmail.com>
2017-12-29 15:08:33 +0000

    countdown Timer function

---
+++ cdt.js
@@ -0,0 +1,15 @@
+function countDown(seconds){
+    var interv = 1000;
+    var intervId = setInterval((){
+        console.log(seconds);
+        seconds-=1;
+
+        if( seconds < 1){
+            console.log("Ring Ring!!");
+            clearInterval(intervId);
+        }
+    }, interv);
+    
+}
+
+countDown(5);
